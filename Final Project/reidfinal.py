b880b124a559e86fc834e86b26ae920d1e788939
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-10-02 14:58:23 -0400

    started final project

--- Final Project/reidfinal.py
+++ Final Project/reidfinal.py
@@ -1,7 +1,50 @@
-import BankManager
-import Account
-import Bank
-import BankUtility
-import CoinCollector
+# import BankManager
+# import Account
+# import Bank
+# import BankUtility
+# import CoinCollector
 
 
+def menu():
+    selection = [
+        "=====================================================",
+        "1. Open an account",
+        "2. Get account informatio and balance",
+        "3. Change PIN",
+        "4. Deposit money in account",
+        "5. Transfer money between accounts",
+        "6. Withdraw money from account",
+        "7. ATM Withdrawl",
+        "8. Deposit change",
+        "9. Close an account",
+        "10. Add monthly interest to all accounts",
+        "11. End Program",
+        "====================================================="
+        ]
+    
+    return selection
+
+
+
+
+
+def main():
+    selection = menu()
+    while True:
+        print("What do you want to do? ")
+        for s in selection:
+            print(s)
+
+        try:
+            option = int(input())
+            if option in range(1, (len(selection) - 1)):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please enter a valid selection.")
+
+
+
+if __name__ == "__main__":
+    main()
