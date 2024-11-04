d11715da4b8eaad0ad9d0773faf8530c2e0e5735
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-11-04 13:27:53 -0500

    sorting algorithms

---
+++ P3_1.py
@@ -0,0 +1,10 @@
+# Jesse Reid CSC6013 Coding Project 3 #1
+
+# Adapt the Selection Sort algorithm seem in class to sort the elements being the largest ones selected to go to the last positions first, instead of the version presented in class, where the smallest ones where selected to go to the first positions. Trace your algorithm execution printing:
+# at each iteration of the outer loop count the number of times two array elements are compared and the number of times two array elements were swapped, plus the current status of the array.
+# test your algorithm for the arrays:
+# A1 = [63, 44, 17, 77, 20, 6, 99, 84, 52, 39]
+# A2 = [84, 52, 39, 6, 20, 17, 77, 99, 63, 44]
+# A3 = [99, 84, 77, 63, 52, 44, 39, 20, 17, 6]
+
+
