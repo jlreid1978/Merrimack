d08c9010caa889a1e029184e04533b74a24a1122
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-11-10 16:16:16 -0500

    coding project 4

---
+++ CSC6013/P4_1.py
@@ -0,0 +1,8 @@
+# Adapt the Binary Search algorithm seem in class to search a given element assuming that the array is not in ascending order, but instead it is sorted in descending order. Trace your algorithm execution printing:
+# at each recursive call, print the subarray from start to end and show the mid element.
+# test your algorithm for the arrays:
+# A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]   searching for 44;
+# A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]   searching for 56;
+# A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]   searching for 42;
+# A2 = [9, 7, 6, 4, 2, 0, -1, -3, -5, -8, -9]   searching for -1;
+# A2 = [9, 7, 6, 4, 2, 0, -1, -3, -5, -8, -9]   searching for -7.
---
+++ CSC6013/P4_2.py
@@ -0,0 +1,7 @@
+# Adapt the Maximum Element in an Array algorithm seem to search for the minimum element in the array. Trace your algorithm execution printing:
+# at the beginning of each recursive call the start and end values.
+# at the returning of each recursive call the returned value (the minimum of the array slice).
+# test your algorithm for the arrays:
+# A3 = [44, 63, 77, 17, 20, 99, 84, 6, 39, 52]
+# A4 = [52, 84, 6, 39, 20, 77, 17, 99, 44, 63]
+# A5 = [6, 17, 20, 39, 44, 52, 63, 77, 84, 99]
