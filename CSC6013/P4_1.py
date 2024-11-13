7d2b78b61f8f8fadc58a2fd87e0ca02bc8e621a0
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-11-13 11:29:46 -0500

    Adding project for binary search and minimum value search

--- CSC6013/P4_1.py
+++ CSC6013/P4_1.py
@@ -1,8 +1,34 @@
-# Adapt the Binary Search algorithm seem in class to search a given element assuming that the array is not in ascending order, but instead it is sorted in descending order. Trace your algorithm execution printing:
-# at each recursive call, print the subarray from start to end and show the mid element.
-# test your algorithm for the arrays:
-# A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]   searching for 44;
-# A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]   searching for 56;
-# A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]   searching for 42;
-# A2 = [9, 7, 6, 4, 2, 0, -1, -3, -5, -8, -9]   searching for -1;
-# A2 = [9, 7, 6, 4, 2, 0, -1, -3, -5, -8, -9]   searching for -7.
+# Jesse Reid CSC6013 coding project P4 #1
+
+def binSearch(A, start, end, k):
+    mid = (end+start)//2
+    # print the mid value based on mid index
+    print(f"Mid: {A[mid]}")
+    if (start > end):
+        return None
+    elif (A[mid] == k):
+        return mid
+    elif (A[mid] > k):
+        # if the value is in the second half, split the array to the end
+        print(f"Subarray: {A[(mid+1):end+1]}")
+        return binSearch(A, mid+1, end, k)
+
+    else:
+        # if the value is in the first half, split the array to the beginning
+        print(f"Subarray: {A[start:mid]}")
+        return binSearch(A, start, mid-1, k)
+    
+
+A1 = [99, 67, 56, 51, 44, 39, 38, 23, 21, 17, 11, 2]
+# print the original array
+print(f"\nOriginal Array: {A1}")
+# search for the requested numbers in the array
+for i in [44, 56, 42]:
+    print("{} is at index {}\n".format(i, binSearch(A1, 0, len(A1)-1, i)))
+
+A2 = [9, 7, 6, 4, 2, 0, -1, -3, -5, -8, -9]
+# print the original array
+print(f"\nOriginal Array: {A2}")
+#search for the requeste numbers in the array
+for i in [-1, -7]:
+    print("{} is at index {}\n".format(i, binSearch(A2, 0, len(A2)-1, i)))
--- CSC6013/P4_2.py
+++ CSC6013/P4_2.py
@@ -1,7 +1,33 @@
-# Adapt the Maximum Element in an Array algorithm seem to search for the minimum element in the array. Trace your algorithm execution printing:
-# at the beginning of each recursive call the start and end values.
-# at the returning of each recursive call the returned value (the minimum of the array slice).
-# test your algorithm for the arrays:
-# A3 = [44, 63, 77, 17, 20, 99, 84, 6, 39, 52]
-# A4 = [52, 84, 6, 39, 20, 77, 17, 99, 44, 63]
-# A5 = [6, 17, 20, 39, 44, 52, 63, 77, 84, 99]
+# Jesse Reid CSC6013 coding project P4 #2
+
+def Min(A, start, end):
+    print(f"Start value: {A[start]}, end value: {A[end]}")
+    if (start == end):
+        return end
+    else:
+        mid = (end + start) // 2
+        fst = Min(A, start, mid)
+        lst = Min(A, mid + 1, end)
+        
+        if A[fst] < A[lst]:
+            print(f"Returning value {A[fst]} from left slice")
+            return fst
+        else:
+            print(f"Returning value {A[lst]} from right slice")
+            return lst
+
+
+A3 = [44, 63, 77, 17, 20, 99, 84, 6, 39, 52]
+print(f"\nOriginal array: {A3}")
+i = Min(A3, 0, len(A3) - 1)
+print("The minimum number is", A3[i], "at index", i)
+
+A4 = [52, 84, 6, 39, 20, 77, 17, 99, 44, 63]
+print(f"\nOriginal array: {A4}")
+i = Min(A4, 0, len(A4) - 1)
+print("The minimum number is", A4[i], "at index", i)
+
+A5 = [6, 17, 20, 39, 44, 52, 63, 77, 84, 99]
+print(f"\nOriginal array: {A5}")
+i = Min(A5, 0, len(A5) - 1)
+print("The minimum number is", A5[i], "at index", i)
