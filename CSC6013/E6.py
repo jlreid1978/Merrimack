5f2ab9431a8929319fd08be6c09968a15a4cde70
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-11-27 11:16:22 -0500

    final Russian peasant and Lomuto

--- CSC6013/E6.py
+++ CSC6013/E6.py
@@ -1,3 +1,5 @@
+# Jesse Reid CSC6013 In Class Exercise E6
+
 # 1. TracetheRussianPeasantsMultiplicationalgorithmforthefollowingproducts. Show each recursive call and the final result, as shown in the live session (table).
 # a. 64*13 b. 60*13 c. 59*13
 
@@ -62,16 +64,68 @@ def russian(n, m):
         ecount += 1
     print("___________\n")
 
-
     print(f"After adding the checked values\n{n} x {m} is {peasant_sum}\n\n")
 
 
+# Lomuto partition
+# 2. TracetheLomutopartitionwiththearray:
+# a. A=[100,33,22,213,65,29,153,199,47,181,85]
+# Using A[10] = 85 as pivot the final array will be:
+# ● A = [33, 22, 65, 29, 47, 85, 153, 199, 100, 181, 213]
+# In your trace, write down to each change in either i or j, stating: the values of i and j, swaps made, 
+# and elements divided into lesser than the pivot, greater than the pivot, and yet to compare.
+
+# recursive function to execute the Lomuto partition
+def lomuto(A, left, right):
+    pivot = A[10]
+    # print the stating array and pivot
+    print(f"Starting array = {A} with {pivot} as the pivot.\n")
+    p = A[right]
+    i = left
+    # itirate through A
+    for j in range(left, right):
+        # print the current status of the array and visited values
+        print(f"Current array {A}")
+        print(f"Elements less than pivot {A[:i]}")
+        print(f"Elements greater than pivot {A[i:j]}")
+        print(f"Elements not visited {A[j:]}\n")
+
+        # print the current values for j and i
+        print(f"j = {A[j]}, i = {A[i]}")
+        
+        if A[j] < p:
+            # indicate that a swap was made
+            print(f"{A[j]} < {p}; swapping {A[j]} with {A[i]}")
+            A[i], A[j] = A[j], A[i]
+
+            i += 1
+            
+        else:
+            # indicate that no swap was made
+            print(f"{A[j]} > {p}; no swap")
+
+    # final swap for the pivot itself
+    A[i], A[right] = A[right], A[i]
+    print(f"Final swap {A[right]} for {A[i]}\n")
+
+    return i
+
 
 def main():
+    print("Jesse Reid CSC6013 In Class Exercise E6\n\n")
+    print("________________________________\nRussian Peasants Multiplication:\n________________________________\n")
     values = {64:13, 60:13, 59:13}
     for n , m in values.items():
         print(f"Multiplying {n} and {m} using the Russian Peasant method")
         russian(n, m)
+
+    print("_________________\nLomuto partition:\n_________________\n")
+    A = [100, 33, 22, 213, 65, 29, 153, 199, 47, 181, 85]
+    pvt = lomuto(A, 0, len(A)-1)
+    print("Lomuto with pivot at", pvt, ":", A[pvt])
+    print(f"Final array {A}")
+    print(f"\nItems less than pivot {A[:pvt]}")
+    print(f"Items greater than pivot {A[pvt+1:]}")
 
 if __name__ == "__main__":
     main() 
