fc8ac36788a6b089804a2436f84b4098559560ce
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-11-27 11:20:51 -0500

    Quick Select script

---
+++ CSC6013/P6.py
@@ -0,0 +1,12 @@
+# You must code a function QuickSelect that receives an array and the element the user wants to find (k-th smallest);
+# Then the main function of your program should generate a random array of 1000 elements to be searched, 
+# ask the user the value of k, call QuickSelect, and display the searched element found.
+
+def QuickSelect(A, k):
+    return k_value
+
+def main():
+    k_value = QuickSelect(A,k)
+
+if __name__ == "__main__":
+    main() 
