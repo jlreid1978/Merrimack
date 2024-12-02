b3b00372c00309d016172a6daaf7819ca490ec47
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2024-12-02 09:25:28 -0500

    E7 Array Sum and Mergesort

---
+++ CSC6013/E7_1.py
@@ -0,0 +1,13 @@
+# Array Sum
+# 1. Tracetherecursivearraysumalgorithmforthefollowingarrays.Showtoeach recursive call the input array, the returned value, and the number of sums executed. At the end of the trace, present the total number of sums executed (the total of sums of all recursive calls.
+# a. A=[38,21,39,60,-1,10,81,23]
+# b. B=[2,97,5,88,9,72,12,64,17,56,21]
+# c. C = [100, 33, 22, 213, 65, 29, 153, 199, 47, 181, 85]
+
+
+def main():
+    return True
+
+
+if __name__ == "__main__":
+    main()
---
+++ CSC6013/E7_2.py
@@ -0,0 +1,13 @@
+# Mergesort
+# 2. TracetheMergesortalgorithmforthefollowingarrays.Showtoeachrecursive call the two input and output arrays.
+# a. A=[38,21,39,60,-1,10,81,23]
+# b. B=[2,97,5,88,9,72,12,64,17,56,21]
+# c. C = [100, 33, 22, 213, 65, 29, 153, 199, 47, 181, 85]
+
+
+def main():
+    return True
+
+
+if __name__ == "__main__":
+    main()
